4cca4fe4341b50892f99007235a0c7080e4248ad
Vivek <92520077+healerXvivek@users.noreply.github.com>
2022-11-18 12:12:59 +0530

    move_all_zeroes_to_end.cpp
    code for move_all_zeroes_to_end

---
+++ ARRAY/move_all_zeroes_to_end.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+	void pushZerosToEnd(int arr[], int n) {
+	    int count=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]!=0)
+	        {
+	        swap(arr[i],arr[count]);
+	        count ++;
+	        }
+	    }
+	}
+};
