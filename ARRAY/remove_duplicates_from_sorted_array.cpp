d9af3bb6f1dcede810b4c4c0bd8d76fb9bfd6d58
Vivek <92520077+healerXvivek@users.noreply.github.com>
2022-11-18 11:52:25 +0530

    remove_duplicates_from_sorted_array.cpp
    code for remove_duplicates_from_sorted_array

---
+++ ARRAY/remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+    int remove_duplicate(int arr[],int n){
+        int res=1;
+        for (int i=1;i<n;i++)
+        {
+            if (arr[i]!=arr[res-1]){
+                arr[res]=arr[i];
+                res++;
+            }
+        }
+        return res;
+    }
+};
