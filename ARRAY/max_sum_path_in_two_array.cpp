a2c993dc0a13afba88f6137feaeaa34f09fe8691
Vivek <92520077+healerXvivek@users.noreply.github.com>
2022-11-18 18:59:23 +0530

    max_sum_path_in_two_array.cpp
    code for max_sum_path_in_two_array

---
+++ ARRAY/max_sum_path_in_two_array.cpp
@@ -0,0 +1,29 @@
+class Solution{
+    public:
+     int max_path_sum(int A[], int B[], int n, int m)
+    {
+        int sum1=0,sum2=0,sum=0;
+        int i=0,j=0;
+        while(i<n&&j<m){
+            if(A[i]==B[j]){
+                sum+=max(sum1,sum2);
+                sum+=A[i];
+                i++;
+                j++;
+                sum1=0;sum2=0;
+            }
+            else if(A[i]<B[j]){
+                sum1+=A[i++];
+            }
+            else{
+                sum2+=B[j++];
+            }
+        }
+        while(i<n){
+            sum1+=A[i++];
+        }
+        while(j<m)sum2+=B[j++];
+        sum+=max(sum1,sum2);
+        return sum;
+    }
+};
