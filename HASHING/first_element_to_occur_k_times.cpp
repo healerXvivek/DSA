457f37e904b705fb7c8b0be8c2e2b3f29ea80996
Vivek <92520077+healerXvivek@users.noreply.github.com>
2022-12-08 15:53:41 +0530

    Create first_element_to_occur_k_times.cpp
    Given an array of N integers. Find the first element that occurs at least K number of times.
     
    
    Example 1:
    
    Input :
    N = 7, K = 2
    A[] = {1, 7, 4, 3, 4, 8, 7}
    Output :
    4
    Explanation:
    Both 7 and 4 occur 2 times. 
    But 4 is first that occurs 2 times
    As at index = 4, 4 has occurred 
    atleast 2 times whereas at index = 6,
    7 has occurred atleast 2 times.
     
    
    Your Task:  
    You don't need to read input or print anything. Your task is to complete the function firstElementKTime() which takes the array A[], its size N, and an integer K as inputs and returns the required answer. If the answer is not present in the array, return -1.
    
    Expected Time Complexity: O(N)
    Expected Auxiliary Space: O(N)
    
    Constraints:
    1 <= N, K <= 105
    1<= A <= 106

---
+++ HASHING/first_element_to_occur_k_times.cpp
@@ -0,0 +1,13 @@
+class Solution{
+    public:
+    int firstElementKTime(int a[], int n, int k)
+    {
+       unordered_map<int,int> mp;
+       for(int i=0;i<n;i++){
+           mp[a[i]]++;
+           if(mp[a[i]] == k)
+           return a[i];
+       }
+       return -1;
+    }
+};
