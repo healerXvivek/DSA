246ee1ce17f65cd482e302d97c03b3aac6c8929f
Vivek <92520077+healerXvivek@users.noreply.github.com>
2022-12-09 11:11:55 +0530

    Create check_subarray_with_given_sum.cpp

---
+++ HASHING/check_subarray_with_given_sum.cpp
@@ -0,0 +1,20 @@
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n, int s)
+    {
+        //Your code here
+        unordered_set<int> s;
+        int ps=0;
+        for(int i=0;i<n;i++){
+            ps+=arr[i];
+            if(s.find(ps-s) != s.end())
+            return 1;
+            if(ps == s)
+            return 1;
+            s.insert(ps);
+        }
+        return 0;
+    }
+};
