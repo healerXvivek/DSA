9204073e79ce74908e9f99799c757f12fdc3dcb6
Vivek <92520077+healerXvivek@users.noreply.github.com>
2022-12-14 21:59:26 +0530

    Create n_meetings_in_one_room.cpp
    There is one meeting room in a firm. There are N meetings in the form of (start[i], end[i]) where start[i] is start time of meeting i and end[i] is finish time of meeting i.
    What is the maximum number of meetings that can be accommodated in the meeting room when only one meeting can be held in the meeting room at a particular time?
    
    Note: Start time of one chosen meeting can't be equal to the end time of the other chosen meeting.
    
    
    Example 1:
    
    Input:
    N = 6
    start[] = {1,3,0,5,8,5}
    end[] =  {2,4,6,7,9,9}
    Output: 
    4
    Explanation:
    Maximum four meetings can be held with
    given start and end timings.
    The meetings are - (1, 2),(3, 4), (5,7) and (8,9)
    Example 2:
    
    Input:
    N = 3
    start[] = {10, 12, 20}
    end[] = {20, 25, 30}
    Output: 
    1
    Explanation:
    Only one meetings can be held
    with given start and end timings.
    
    Your Task :
    You don't need to read inputs or print anything. Complete the function maxMeetings() that takes two arrays start[] and end[] along with their size N as input parameters and returns the maximum number of meetings that can be held in the meeting room.
    
    
    Expected Time Complexity : O(N*LogN)
    Expected Auxilliary Space : O(N)
    
    
    Constraints:
    1 ≤ N ≤ 105
    0 ≤ start[i] < end[i] ≤ 105

---
+++ GREEDY/n_meetings_in_one_room.cpp
@@ -0,0 +1,26 @@
+# https://practice.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1
+
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    int maxMeetings(int start[], int end[], int n)
+    {
+        vector<pair<int,int>> v;
+        for(int i=0;i<n;i++)
+        v.push_back({end[i],start[i]});
+        
+        sort(v.begin(),v.end());
+        int k=v[0].first;
+        int ans=1;
+        
+        for(int i=1;i<v.size();i++){
+            if(v[i].second > k){
+                ans++;
+                k=v[i].first;
+            }
+        }
+        return ans;
+    }
+};
