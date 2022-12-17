9b174988ec7721ccb9d2b115b74213cd54d383a1
Vivek <92520077+healerXvivek@users.noreply.github.com>
2022-12-17 13:36:59 +0530

    Create intersection_point_in_y_shaped_linked_list.cpp
    Given two singly linked lists of size N and M, write a program to get the point where two linked lists intersect each other.
    
     
    
    Example 1:
    
    Input:
    LinkList1 = 3->6->9->common
    LinkList2 = 10->common
    common = 15->30->NULL
    Output: 15
    Explanation:
    Y ShapedLinked List
    Example 2:
    
    Input: 
    Linked List 1 = 4->1->common
    Linked List 2 = 5->6->1->common
    common = 8->4->5->NULL
    Output: 8
    Explanation: 
    
    4              5
    |              |
    1              6
     \             /
      8   -----  1 
       |
       4
       |
      5
      |
      NULL       
    Your Task:
    You don't need to read input or print anything. The task is to complete the function intersetPoint() which takes the pointer to the head of linklist1(head1) and linklist2(head2) as input parameters and returns data value of a node where two linked lists intersect. If linked list do not merge at any point, then it should return -1.
    Challenge : Try to solve the problem without using any extra space.
    
     
    
    Expected Time Complexity: O(N+M)
    Expected Auxiliary Space: O(1)
    
     
    
    Constraints:
    1 ≤ N + M ≤ 2*10^5
    -1000 ≤ value ≤ 1000

---
+++ LINKED LIST/intersection_point_in_y_shaped_linked_list.cpp
@@ -0,0 +1,48 @@
+# https://practice.geeksforgeeks.org/problems/intersection-point-in-y-shapped-linked-lists/1
+
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+//Function to find intersection point in Y shaped Linked Lists.
+int intersectPoint(Node* head1, Node* head2)
+{
+    int c1 = 0 , c2 = 0;
+    Node *curr1 = head1 , *curr2 = head2;
+    
+     // calculating lengths
+     while(curr1 != NULL)
+     {
+       curr1 = curr1->next; c1++;
+     }
+     
+     while(curr2 != NULL)
+     {
+       curr2 = curr2->next; c2++;
+     }
+     int diff = abs(c2-c1);
+     // traversing the bigger list
+      if(c1 >c2)
+      {
+        for(int i=0;i<diff;i++)
+        head1 = head1->next;
+      }
+      else
+      {
+            for(int i=0;i<diff;i++)
+              head2 = head2->next;
+      }
+      //finding the common node
+      while(head1 != head2)
+      {
+            head1 = head1->next;
+            head2 = head2->next;
+      }
+      return head1->data;
+}
