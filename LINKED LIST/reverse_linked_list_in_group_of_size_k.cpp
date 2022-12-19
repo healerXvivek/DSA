a0c3b1a3f4339ca9888ae2e23c2a0dd14d5611af
Vivek <92520077+healerXvivek@users.noreply.github.com>
2022-12-19 20:29:53 +0530

    Create reverse_linked_list_in_group_of_size_k.cpp
    Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should be considered as a group and must be reversed (See Example 2 for clarification).
    
    Example 1:
    
    Input:
    LinkedList: 1->2->2->4->5->6->7->8
    K = 4
    Output: 4 2 2 1 8 7 6 5 
    Explanation: 
    The first 4 elements 1,2,2,4 are reversed first 
    and then the next 4 elements 5,6,7,8. Hence, the 
    resultant linked list is 4->2->2->1->8->7->6->5.
    Example 2:
    
    Input:
    LinkedList: 1->2->3->4->5
    K = 3
    Output: 3 2 1 5 4 
    Explanation: 
    The first 3 elements are 1,2,3 are reversed 
    first and then elements 4,5 are reversed.Hence, 
    the resultant linked list is 3->2->1->5->4.
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function reverse() which should reverse the linked list in group of size k and return the head of the modified linked list.
    
    Expected Time Complexity : O(N)
    Expected Auxilliary Space : O(1)
    
    Constraints:
    1 <= N <= 10^4

---
+++ LINKED LIST/reverse_linked_list_in_group_of_size_k.cpp
@@ -0,0 +1,42 @@
+#
+
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        node *curr=head;
+        node *prev=NULL,*next=NULL;
+        int count=0;
+        while(curr!=NULL && count<k){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            count++;
+        }
+        if(next!=NULL){
+            node *rest_head=reverse(next,k);
+            head->next=rest_head;
+        }
+        return prev;
+    }
+};
