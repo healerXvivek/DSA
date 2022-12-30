44b93b6baf643b229d59c8ef03a6ea8fc13329d8
Vivek <92520077+healerXvivek@users.noreply.github.com>
2022-12-30 23:57:48 +0530

    Create odd_occuring_element.cpp
    Given an array of N positive integers where all numbers occur even number of times except one number which occurs odd number of times. Find the exceptional number.
    
    Example 1:
    
    Input:
    N = 7
    Arr[] = {1, 2, 3, 2, 3, 1, 3}
    Output: 3
    Explaination: 3 occurs three times.
    
    
    Example 2:
    
    Input:
    N = 7
    Arr[] = {5, 7, 2, 7, 5, 2, 5}
    Output: 5
    Explaination: 5 occurs three times.
    
    
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function getOddOccurrence() which takes arr[] and n as input parameters and returns the exceptional number.
    
    
    Expected Time Complexity: O(N)
    Expected Auxiliary Space: O(1)
    
    
    Constraints:
    1 ≤ N ≤ 10^5
    1 ≤ arr[i] ≤ 10^6
    
    https://practice.geeksforgeeks.org/problems/find-the-odd-occurence4820/1

---
+++ BIT MAGIC/odd_occuring_element.cpp
@@ -0,0 +1,11 @@
+class Solution{   
+public:
+    int getOddOccurrence(int arr[], int n) {
+        // code here
+        int res=0;
+        for(int i=0;i<n;i++)
+        res=res^arr[i];
+        
+        return res;
+    }
+}
