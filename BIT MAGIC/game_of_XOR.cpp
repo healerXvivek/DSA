f10f19f41e8cdddcc5e4a738b213775d445e28e6
Vivek <92520077+healerXvivek@users.noreply.github.com>
2022-12-31 12:55:42 +0530

    Create  game_of_XOR.cpp
    Given an array A[] of size N. The value of an array is denoted by the bit-wise XOR of all elements it contains. Find the bit-wise XOR of the values of all subarrays of A[].
    
    Example 1:
    
    Input: N = 3, A[] = {1,2,3} 
    Output: 2
    Explanation:
    xor[1] = 1, 
    xor[1, 2] = 3
    xor[2, 3] = 1
    xor[1, 2, 3] = 0
    xor[2] = 2
    xor[3] = 3
    Result : 1 ^ 3 ^ 1 ^ 0 ^ 2 ^ 3 = 2
    
    Example 2:
    
    Input: N = 2, A[] = {1,2} 
    Output: 0
    Explanation:
    xor[1] = 1, 
    xor[1, 2] = 3
    xor[2] = 2
    Result : 1 ^ 3 ^ 2 = 0
     
    
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function gameOfXor() which takes an integer N and array A[] as input parameters and returns the answer.
    
     
    
    Expected Time Complexity: O(N)
    Expected Auxiliary Space: O(1)
    
     
    
    Constraints:
    1 <= N <= 10^5
    1 <= A[i ]<= 10^5
    
    https://practice.geeksforgeeks.org/problems/game-of-xor1541/1

--- BIT MAGIC/game_of_XOR.cpp
+++ BIT MAGIC/game_of_XOR.cpp
@@ -1,3 +1,28 @@
+/*
+HINT 1
+If an element is xor with itself, even number of times, it becomes 0.
+If an element is xor with itself, odd number of times, it remains the same.
+For example
+(A^A)^2n   = 0            // even no of times
+(A^A)^2n+1  = A        // odd no of times
+
+HINT 2
+For array [1,2,3] its subarrays will be
+1
+1 2
+2
+3
+1 2 3
+2 3
+
+Here a particular ith index repeates (N-i) * (i+1) times.
+Now if we see carefully (N-i) or (i+1) is always an even number for all values of i's possible.
+
+HINT 3
+For even length xor of all subarrays will be 0 and for odd length xor of all subarrays will be xor of all even index elements.
+
+*/
+
 class Solution {
   public:
     int gameOfXor(int N , int A[]) {
