6d391121e46d5acb8fc175d1b1aeed6aacd90f3c
Vivek <92520077+healerXvivek@users.noreply.github.com>
2023-01-01 18:59:47 +0530

    Create power_set.cpp

---
+++ BIT MAGIC/power_set.cpp
@@ -0,0 +1,21 @@
+class Solution{
+	public:
+		vector<string> AllPossibleStrings(string s){
+		    int n=s.length();
+		    vector<string> v;
+		    int pow_size=pow(2,n);
+		    
+		    for(int counter=0; counter < pow_size; counter++){
+		        string str="";
+		        for(int j=0;j<n;j++){
+                    if(counter & (1 << j)){
+                    str+=s[j];
+                }
+		    }
+		    if(str.size()!=0)
+		    v.push_back(str);
+		    }
+		    sort(v.begin(),v.end());
+		    return v;
+		}
+};
