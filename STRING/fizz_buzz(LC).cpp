adb29318a3345862dd2140b34697f8107609b15a
Vivek <92520077+healerXvivek@users.noreply.github.com>
2023-01-03 18:50:09 +0530

    Create fizz_buzz(LC).cpp
    Given an integer n, return a string array answer (1-indexed) where:
    
    answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
    answer[i] == "Fizz" if i is divisible by 3.
    answer[i] == "Buzz" if i is divisible by 5.
    answer[i] == i (as a string) if none of the above conditions are true.
     
    
    Example 1:
    
    Input: n = 3
    Output: ["1","2","Fizz"]
    Example 2:
    
    Input: n = 5
    Output: ["1","2","Fizz","4","Buzz"]
    Example 3:
    
    Input: n = 15
    Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
     
    
    Constraints:
    
    1 <= n <= 10^4
    
    https://leetcode.com/problems/fizz-buzz/description/

---
+++ STRING/fizz_buzz(LC).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> v(n);
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0)
+            v[i-1]="FizzBuzz";
+            else if(i%3==0)
+            v[i-1]="Fizz";
+            else if(i%5==0)
+            v[i-1]="Buzz";
+            else
+            v[i-1]=to_string(i);
+        }
+        return v;
+    }
+};
