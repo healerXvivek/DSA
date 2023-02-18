60a548d8fe533aa05f75637c907c58c40bca7282
Vivek <92520077+healerXvivek@users.noreply.github.com>
2023-02-18 11:25:04 +0530

    Create height_of_binary_tree.cpp
    Given a binary tree, find its height.
    
    
    Example 1:
    
    Input:
         1
        /  \
       2    3
    Output: 2
    Example 2:
    
    Input:
      2
       \
        1
       /
     3
    Output: 3   
    
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function height() which takes root node of the tree as input parameter and returns an integer denoting the height of the tree. If the tree is empty, return 0. 
    
    
    Expected Time Complexity: O(N)
    Expected Auxiliary Space: O(N)
    
    
    Constraints:
    1 <= Number of nodes <= 105
    1 <= Data of a node <= 105
    
    https://practice.geeksforgeeks.org/problems/height-of-binary-tree/1

---
+++ TREE/height_of_binary_tree.cpp
@@ -0,0 +1,23 @@
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* node){
+        if(node == NULL)
+        return 0;
+        else
+        return max(height(node->left),height(node->right))+1;
+    }
+};
